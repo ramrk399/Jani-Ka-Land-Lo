f1cca76cd29d66344e77fc69c845effb86da1d16
ramrk399 <ramavtarchauhan2000@gmail.com>
2024-08-12 15:01:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/istkharalam62/PurviMusic",
+    "https://github.com/ramrk399/Jani-Ka-Land-Lo",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
